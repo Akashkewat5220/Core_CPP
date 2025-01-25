4364a964d66fa1a91aad16b5dc9c2c92ae8a8239
Akashkewat5220 <kewatakash42@gmail.com>
2025-01-26 01:03:33 +0530

    update readme

--- 24th_Jan/Code.cpp
+++ 24th_Jan/Code.cpp
@@ -2,15 +2,30 @@
 
 #include<iostream>
 using namespace std ;
+#include <bits/stdc++.h>
 
 int main() 
 {
-    int arr[5] = {1,2,3,4,5} ;
-    cout<< arr[2] <<endl ;  // indexing 
+    // int arr[5] = {1,2,3,4,5} ;
+    // cout<< arr[2] <<endl ;  // indexing 
 
     //iteration
-    for(int i = 0 ;i < 5 ; i++)
-    {
-        cout<< arr[i]<< endl ;
-    }
+    // for(int i = 0 ;i < 5 ; i++)
+    // {
+    //     cout<< arr[i]<< endl ;
+    // }
+
+    // 2D __ARRAY_OPERATORSint 
+    // int arr[2][3] = {{1,2,3}, {4,5,6}} ;
+
+    // VECTORS
+    // size fixed then use array 
+    // else use vector
+
+    // syntax = :
+    // vector<int>v(10, 3)
+    // vector<int>v = {1,2,3,4} ;
+    // cout<<v.size() ;
+    // v.front()
+    // v.back() 
 }
