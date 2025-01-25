a8920cd72f262d341139767ce222622aa599a438
Akashkewat5220 <kewatakash42@gmail.com>
2025-01-25 23:54:39 +0530

    update readme

---
+++ 24th_Jan/Code.cpp
@@ -0,0 +1,16 @@
+// TOPICS COVERED  = Arrays, 
+
+#include<iostream>
+using namespace std ;
+
+int main() 
+{
+    int arr[5] = {1,2,3,4,5} ;
+    cout<< arr[2] <<endl ;  // indexing 
+
+    //iteration
+    for(int i = 0 ;i < 5 ; i++)
+    {
+        cout<< arr[i]<< endl ;
+    }
+}
