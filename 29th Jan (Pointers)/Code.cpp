8233eea44c1cb2cf6d7b6e756dc275c7cce7ad3a
Akashkewat5220 <kewatakash42@gmail.com>
2025-02-09 13:51:12 +0530

    update readme

---
+++ 29th Jan (Pointers)/Code.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    // declaration 
+    // int var =  5  ;
+    // int *ptr = &var ;
+
+    // cout<< var <<endl ;
+    // cout<< &var << endl ;
+    // cout<< ptr <<endl ;
+    // cout<< *ptr <<endl ;
+
+    // POINTERS ARITHMETIC
+    // int arr[] = {1,2,3,4,5} ;
+    // int *ptr = arr ;
+    // cout<< ptr<<endl ;
+    // cout<< *ptr <<endl ;
+    // ptr++ ; 
+    // cout<< *ptr ;
+
+    // NULL POINTER
+    // int num = 7  ;
+    // int *ptr = nullptr ;
+
+    // VOID POINTER
+    // void *ptr ;
+    // int num =  8 ; 
+    // int *iptr ;
+    // *iptr = (int *)&num ;
+    // cout<< *iptr <<endl ;
+
+    // POINTER TO POINTER 
+    // int num = 5 ;
+    // int *ptr = &num ;
+    // int **ptr2 = &ptr ;
+
+    // DYNAMIC ALLOCATION
+    // int *ptr = new int(10) ;
+    // cout<<*ptr <<endl ;
+    // delete ptr ;
+
+    // POINTERS AND ARRAYS
+    // int arr[] = {1,2,3,4,5} ;
+    // int *ptr = arr ;
+    // cout<< *(ptr + 1) ;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0 ;  
+}
